940d64740302c567604378e1af773baeb5c78d70
Rishi <rishipradhan896@gmail.com>
2023-04-16 01:43:57 +0530

    Update 0.1

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 
 const Portfolio = () => {
+
+
+  const openClickbuzz = () => {
+    window.open('https://vermillion-capybara-bb2033.netlify.app/', '_blank');
+
+} 
+
+
   return (
     <div>
       <section class="text-gray-400 body-font bg-gray-900">
@@ -21,35 +29,33 @@ const Portfolio = () => {
           <section class="text-gray-600 body-font overflow-hidden">
             <div class="container px-5 py-24 mx-auto">
               <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                <img
+             <img
                   alt="ecommerce"
                   class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                  src="https://source.unsplash.com/720x400/?Flipkart "
-                />
+                 src="../image/Clickbuzz.png"
+                /> 
                 <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                   <h2 class="text-sm title-font text-gray-500 tracking-widest">
                     Project 1
                   </h2>
                   <h1 class="text-white text-3xl title-font font-medium mb-4">
-                    Flipkart Clone
+                    ClickBuzz
                   </h1>
 
                   <p class="leading-relaxed">
-                    Fam locavore kickstarter distillery. Mixtape chillwave
-                    tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam
-                    indxgo juiceramps cornhole raw denim forage brooklyn.
-                    Everyday carry +1 seitan poutine tumeric. Gastropub blue
-                    bottle austin listicle pour-over, neutra jean shorts keytar
-                    banjo tattooed umami cardigan.
-                  </p>
+                  ClickBuzz is a visually stunning portfolio website for a photographer, built using React.js, Bootstrap.
+                    <br /> <br /><a href="https://github.com/Rishi6743/Portfolio.git"><i class="fa-brands fa-github fa-flip fa-2xl"></i></a> </p>
+                    
+
                   <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
                   <div class="flex">
                     <span class="title-font font-medium text-2xl text-gray-900">
                       $58.00
                     </span>
-                    <button class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
-                      Visit the Clone Flipkart website{" "}
+                  <button onClick={openClickbuzz}  class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
+                      Check it
                     </button>
+                    
                   </div>
                 </div>
               </div>
