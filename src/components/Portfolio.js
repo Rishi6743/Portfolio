9d17cf00cb8b150bcd7a99a058156a69c8c94ab2
Rishi <rishipradhan896@gmail.com>
2023-04-20 14:47:35 +0530

    Pjoject & Cv Css updated

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = () => {
 
 
   const openClickbuzz = () => {
-    window.open('https://vermillion-capybara-bb2033.netlify.app/', '_blank');
+    window.open('https://clickbuzz.netlify.app/', '_blank');
 
 } 
 
@@ -50,7 +50,7 @@ const Portfolio = () => {
                   <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
                   <div class="flex">
                     <span class="title-font font-medium text-2xl text-gray-900">
-                      $58.00
+                 
                     </span>
                   <button onClick={openClickbuzz}  class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
                       Check it
@@ -63,39 +63,7 @@ const Portfolio = () => {
           </section>
         </div>
       </section>
-      <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="lg:w-4/5 mx-auto flex flex-wrap">
-            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-              <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                Project 2
-              </h2>
-              <h1 class="text-white text-3xl title-font font-medium mb-4">
-                Amazon Cloun
-              </h1>
-
-              <p class="leading-relaxed mb-4">
-                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                sriracha taximy chia microdosing tilde DIY. XOXO fam iligo
-                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-                seitan poutine tumeric. Gastropub blue bottle austin listicle
-                pour-over, neutra jean.
-              </p>
-
-              <div class="flex">
-                <button class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
-                  Visit the Amazon Flipkart website
-                </button>
-              </div>
-            </div>
-            <img
-              alt="ecommerce"
-              class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-              src="https://source.unsplash.com/720x400/?Amazon"
-            />
-          </div>
-        </div>
-      </section>
+     
     </div>
   );
 };
